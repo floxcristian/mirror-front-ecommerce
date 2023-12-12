61c98d346ec1947457919be8131cd1b02b6eb4a2
Fabio Alderete <fabio.alderete@implementos.cl>
2023-12-12 16:11:55 -0300

    se traspasa endpoint categorias y se eliminan componentes no utilizados

--- src/app/shared/interfaces/navigation-link.ts
+++ src/app/shared/interfaces/navigation-link.ts
@@ -1,7 +1,8 @@
 import { Link } from './link';
 import { Menu } from './menu';
 import { Megamenu } from './megamenu';
+import { IThirdLvl } from '@core/models-v2/cms/categories-response.interface';
 
 export interface NavigationLink extends Link {
-  menu?: Megamenu | Menu;
+  menu?: Megamenu | Menu | IThirdLvl[];
 }
