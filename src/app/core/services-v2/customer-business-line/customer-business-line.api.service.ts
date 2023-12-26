a803db8ff7b3355565843234d30908d1c3f9417e
Cristian Flores <cristianflores.ee@gmail.com>
2023-12-26 17:26:17 -0300

    TF-260:WIP:quitando dependencias antiguas

--- src/app/core/services-v2/customer-business-line/customer-business-line.api.service.ts
+++ src/app/core/services-v2/customer-business-line/customer-business-line.api.service.ts
@@ -1,10 +1,12 @@
 // Angular
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+// Rxjs
+import { Observable } from 'rxjs';
 // Env
 import { environment } from '@env/environment';
+// Models
 import { IBusinessLine } from './business-line.interface';
-import { Observable } from 'rxjs';
 
 const API_CUSTOMER = `${environment.apiEcommerce}/api/v1/customer`;
 
@@ -15,7 +17,7 @@ export class CustomerBusinessLineApiService {
   constructor(private http: HttpClient) {}
 
   /**
-   * Obtener todos los giros.
+   * Obtener giros.
    */
   getBusinessLines(): Observable<IBusinessLine[]> {
     return this.http.get<IBusinessLine[]>(`${API_CUSTOMER}/business-lines`);
