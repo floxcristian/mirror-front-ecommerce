63cf774ca7e0ebbd1a1f4a0aa5b5e96054226986
José Espinoza <espinozasalas.jose@gmail.com>
2024-01-02 11:43:29 -0300

    Fix config file

--- src/app/core/config/config.service.ts
+++ src/app/core/config/config.service.ts
@@ -1,19 +1,32 @@
 // Angular
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, Optional, PLATFORM_ID } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 // Models
 import { IConfig } from './config.interface';
 // Rxjs
 import { Observable, tap } from 'rxjs';
+import { REQUEST } from '@nguniversal/express-engine/tokens';
+import { Request } from 'express';
+import { isPlatformServer } from '@angular/common';
 
 @Injectable()
 export class ConfigService {
   private config!: IConfig;
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    @Inject(PLATFORM_ID) private platformId: Object,
+    @Optional() @Inject(REQUEST) private request: Request
+  ) {}
 
   loadConfig(): Observable<IConfig> {
-    return this.http.get<IConfig>('./assets/config/config.json').pipe(
+    let filePath = './assets/config/config.json';
+    if (isPlatformServer(this.platformId) && this.request) {
+      filePath = this.getFullUrl() + '/assets/config/config.json';
+    }
+    console.log('configPath: ' + filePath);
+
+    return this.http.get<IConfig>(filePath).pipe(
       tap((config) => {
         this.config = config;
         console.log('config: ', config);
@@ -23,5 +36,11 @@ export class ConfigService {
 
   getConfig(): IConfig {
     return this.config;
+  }
+
+  private getFullUrl() {
+    const protocol = this.request.protocol; // 'http' or 'https'
+    const host = this.request.headers.host;
+    return `${protocol}://${host}`; // Constructs the full URL
   }
 }
