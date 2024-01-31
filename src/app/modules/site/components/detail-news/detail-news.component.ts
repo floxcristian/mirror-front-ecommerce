0c98d898c9518fb93941cffd7c3652d5721d4f55
Fabio <fabio_alderete_s@hotmail.com>
2024-01-31 15:28:40 -0300

    Auto stash before merge of "new-apis" and "origin/new-apis"

--- src/app/modules/site/components/detail-news/detail-news.component.ts
+++ src/app/modules/site/components/detail-news/detail-news.component.ts
@@ -1,5 +1,4 @@
-// Angular
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 // Services
@@ -15,10 +14,11 @@ export class DetailNewsComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private sanitizer: DomSanitizer,
-    private readonly cmsService: CmsService
-  ) {
-    console.log('blog detail');
-  }
+    private renderer:Renderer2,
+    //ServicesV2
+    private readonly cmsService:CmsService
+  ) {}
+  noticia: any = {};
 
   ngOnInit(): void {
     const postId = this.route.snapshot.params['id'];
@@ -30,51 +30,47 @@ export class DetailNewsComponent implements OnInit {
           /<h4>/g,
           `<h4 style="font-size:19px !important">`
         );
-        this.noticia.text = this.prueba(this.noticia.text);
+        this.noticia.text = this.generarIframe();
       },
       error: (err) => {
         console.log(err);
       },
     });
   }
-
-  prueba(html: any): SafeHtml {
+  generarIframe() {
     const embed = this.noticia.text;
-
     const parentEmbed = this.stringToHTML(embed);
 
-    let oldIframe: any = parentEmbed.querySelectorAll('oembed');
+    let oldIframe:any = parentEmbed.querySelectorAll('oembed')
     oldIframe = Array.from(oldIframe);
 
     for (const i in oldIframe) {
       //Get the url from oembed tag
       let url = oldIframe[i].getAttribute('url');
-      //let heigth = oldIframe;
       //Replace 'watch?v' with 'embed/'
       url = url.replace('watch?v=', 'embed/');
       url = url.split('&t=5');
 
       //Create a iframe tag
-      const newIframe = document.createElement('iframe');
-      newIframe.setAttribute('width', '100%');
-      newIframe.setAttribute('height', '500px;');
-      newIframe.setAttribute('allowFullScreen', '');
-      newIframe.setAttribute('frameBorder', '0');
+      const newIframe = this.renderer.createElement('iframe')
+      this.renderer.setAttribute(newIframe, 'width', '100%');
+      this.renderer.setAttribute(newIframe, 'height', '500px');
+      this.renderer.setAttribute(newIframe, 'allowFullScreen', '');
+      this.renderer.setAttribute(newIframe, 'frameBorder', '0');
       if (url) {
-        newIframe.setAttribute('src', url[0]);
+        this.renderer.setAttribute(newIframe, 'src', url[0]);
       }
       // replace oldIframe with newIframe
       oldIframe[i].parentNode.replaceChild(newIframe, oldIframe[i]);
     }
 
     const contentToRender = parentEmbed.outerHTML;
-    console.log(contentToRender);
     return this.sanitizer.bypassSecurityTrustHtml(contentToRender);
   }
 
-  stringToHTML(str: any): HTMLSpanElement {
-    const domContainer = document.createElement('span');
-    domContainer.innerHTML = str;
+  stringToHTML(str: string):HTMLElement {
+    const domContainer = this.renderer.createElement('span');
+    this.renderer.setProperty(domContainer,'innerHTML',str)
     return domContainer;
   }
 }
