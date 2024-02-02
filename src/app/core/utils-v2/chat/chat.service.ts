437a4c2376ddcd9404f064543abc136d79f146df
Cristian Flores <cristianflores.ee@gmail.com>
2024-02-02 16:16:25 -0300

    add chat folder

--- src/app/core/utils-v2/chat/chat.service.ts
+++ src/app/core/utils-v2/chat/chat.service.ts
@@ -1,6 +1,30 @@
+// Angular
 import { Injectable } from '@angular/core';
+// Env
+import { environment } from '@env/environment';
+// Services
+import { ScriptService } from '../script/script.service';
 
 @Injectable({
   providedIn: 'root',
 })
-export class ChatService {}
+export class ChatService {
+  private readonly webChatScript = environment.webChatScript;
+
+  constructor(private readonly scriptService: ScriptService) {}
+
+  /**
+   * Cargar script de chat.
+   */
+  loadChatScript() {}
+
+  /**
+   * Mostrar botón de chat.
+   */
+  showChatButton() {}
+
+  /**
+   * Ocultar botón de chat.
+   */
+  hideChatButton() {}
+}
