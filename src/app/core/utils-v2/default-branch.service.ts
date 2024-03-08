816a7e76769b2f9810448ebe70c34550e32cb400
José Espinoza <espinozasalas.jose@gmail.com>
2024-03-08 15:21:21 -0300

    Removed console.log in default branch

--- src/app/core/utils-v2/default-branch.service.ts
+++ src/app/core/utils-v2/default-branch.service.ts
@@ -2,12 +2,6 @@ import { environment } from '@env/environment';
 
 export class DefaultBranch {
   static getBranchCode(branchCode?: string): string {
-    console.log(
-      'getBranchCode',
-      branchCode,
-      environment.defaultBranch.code,
-      branchCode ? branchCode : environment.defaultBranch.code
-    );
     return branchCode ? branchCode : environment.defaultBranch.code;
   }
 }
